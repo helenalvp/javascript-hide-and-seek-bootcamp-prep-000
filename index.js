478eeb46213bbb2fb7092c539accb1edeb4e93f0
Helena Liliana Vaquera <helenalvp@gmail.com>
2020-10-21 21:43:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ function getFirstSelector(selector) {
   return document.querySelector(selector)
 }
 
+/* nested target
 function nestedTarget(target){
   document.getElementById(target)
+}
+*/
+function deepestChild() {
+  const lis = document
+  .getElementById('app')
+  .querySelectorAll('ul.ranked-list li');
+ 
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = (i + 1).toString();
+}
+return querySelector("ul.ranked-list li")
 }
